03428c39a8a690fe04deb16e5d0cac1d85dc1b4c
David Swatzell <thatsstrikethree@gmail.com>
2024-08-05 11:00:46 -0500

    add intercepts for cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('URL Shortener', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+      fixture: 'urls.json'
+    }).as('getUrls');
+
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 201,
+      body: {
+        id: 2,
+        long_url: 'https://www.google.com',
+        short_url: 'http://localhost:3001/useshorturl/2',
+        title: 'Google'
+      }
+    }).as('saveUrl');
+
+    cy.visit('http://localhost:3000');
+  });
+
+  
